cd0caf6d6a593451e89374939abd942e8eab5ad1
Clément POIRET <poiret.clement@outlook.fr>
2019-08-11 10:49:48 +0200

    Switched to GNU AGPLv3

---
+++ cpai.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""CPAI (for CryptoCurrency Prediction IA), is developed to try to predict
+future prices (or at least trends) of CryptoCurrencies.
+Copyright (C) 2019  Clément POIRET
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+For any questions, contact me at poiret.clement[at]outlook[dot]fr"""
+
+# Here comes your imports
+
+# Here comes your (few) global variables
+
+# Here comes your class definitions
+
+# Here comes your function definitions
+
+
+def main():
+    """Launcher."""
+    # init the GUI or anything else
+    pass
+
+
+if __name__ == "__main__":
+    main()
