1a027f1fb3243aa91c5071d17da92b853ac301df
Clément POIRET <poiret.clement@outlook.fr>
2019-08-11 19:35:03 +0200

    Added Ichimoku Kinko Hyo

---
+++ utils/helpers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""This module is used to provide various helpers
+such as an imputer to deal with missing values"""
+
+# Importing libraries
+import numpy as np
+import pandas as pd
+
+
+def impute_ts(X):
+    raise NotImplementedError
--- utils/technicalanalysis/indicators.py
+++ utils/technicalanalysis/indicators.py
@@ -10,8 +10,22 @@ import talib
 
 
 # Functions
-def ichimoku(X):
-    raise NotImplementedError
+def ichimoku(X, shift=26):
+    nine_period_high = X.high.rolling(window=9).max()
+    nine_period_low = X.low.rolling(window=9).max()
+    tenkan_sen = (nine_period_high + nine_period_low) / 2
+
+    period26_high = X.high.rolling(window=26).max()
+    period26_low = X.low.rolling(window=26).max()
+    kijun_sen = (period26_high + period26_low) / 2
+
+    senkou_span_a = ((tenkan_sen + kijun_sen) / 2).shift(shift)
+
+    period52_high = X.high.rolling(window=52).max()
+    period52_low = X.low.rolling(window=52).max()
+    senkou_span_b = ((period52_high + period52_low) / 2).shift(shift)
+
+    return tenkan_sen, kijun_sen, senkou_span_a, senkou_span_b
 
 
 def MACD(X):
@@ -19,7 +33,9 @@ def MACD(X):
 
 
 def bollinger(X):
-    raise NotImplementedError
+    #taking closing prices
+    #returning (bolu, ma, bold)
+    return talib.BBANDS(X)
 
 
 def fourrier(X):
